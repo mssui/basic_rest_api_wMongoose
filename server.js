1b35faf7f8cf8402a74fe0a60aa89ce80c67278d
ASLI ARI <asliari2012@gmail.com>
2018-12-07 13:12:36 +0100

    Create post model for Mongo

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+const port = process.env.PORT || 3030;
+mongoose.connect('mongodb://localhost/siznasil');
+
+var app = express();
+
+app.use(cors());
+app.use(bodyparser.json());
+
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// })
+app.use('/', require('./routes/api'));
+
+app.listen(port, ()=>{
+    console.log('Server is running on:', port)
+});
